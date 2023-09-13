7d4e30f1eea93763dfb3071fb99e736361ee34a2
Mark Kraus <vindevccm@gmail.com>
2023-09-13 12:10:16 -0400

    init

--- create-iam-groups.sh
+++ create-iam-groups.sh
@@ -9,6 +9,6 @@ ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
 aws iam create-group --group-name admins
 aws iam create-group --group-name mfa
-aws iam create-policy --policy-name mfa-policy --policy-document file://mfa-policy.json
+aws iam create-policy --policy-name mfa --policy-document file://mfa-policy.json
 aws iam attach-group-policy --group-name mfa --policy-arn arn:aws:iam::$ACCOUNT_ID:policy/mfa
 aws iam attach-group-policy --group-name admins --policy-arn arn:aws:iam::aws:policy/AdministratorAccess
